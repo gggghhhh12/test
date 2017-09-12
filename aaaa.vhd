21290b666d1b71ed35792b889bd5f2055539fa1e
zhangkunkun666 <zhangkunkun666666@163.com>
2017-09-12 21:33:51 +0800

    some changes

---
+++ aaaa.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    09:54:57 05/05/2017 
+-- Design Name: 
+-- Module Name:    aaaa - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity aaaa is
+    Port ( addr : in  STD_LOGIC;
+           e : in  STD_LOGIC);
+end aaaa;
+
+architecture Behavioral of aaaa is
+
+begin
+
+
+end Behavioral;
+
